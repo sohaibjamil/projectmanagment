b6b50dec48913d09ea21c795360218ce9744f93b
sohaib jamil <sohaibchouhdary@gmail.com>
2016-01-18 14:05:03 +0000

    update create_articles

--- db/migrate/20160118131205_create_articles.rb
+++ db/migrate/20160118131205_create_articles.rb
@@ -1,8 +1,6 @@
 class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
-      t.string :idea, :summary, :description
-      t.timestamps
     end
   end
 end

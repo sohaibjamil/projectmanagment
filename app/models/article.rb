7e2dc3c853f1b08f47d89cede714d7428a48ff40
sohaib jamil <sohaibchouhdary@gmail.com>
2016-01-22 15:26:38 +0000

    delete database files

--- app/models/article.rb
+++
@@ -1,6 +0,0 @@
-class Article <ActiveRecord::Base
-  has_many :comments, dependent: :destroy
-  validate :title, presence: true,
-                   length: { minimum: 5 }
-  belongs_to :user
-end
--- db/migrate/20160118131205_create_articles.rb
+++
@@ -1,6 +0,0 @@
-class CreateArticles < ActiveRecord::Migration
-  def change
-    create_table :articles do |t|
-    end
-  end
-end
